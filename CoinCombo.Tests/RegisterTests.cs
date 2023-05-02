0e9a4b5687a06add475eb6e0fbcf92f5acebfb8c
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-02 15:42:37 -0700

    add CoinCounter static method
     Co-authored-by: Kyle Crawford <kcraw217@gmail.com>

--- CoinCombo.Tests/RegisterTests.cs
+++ CoinCombo.Tests/RegisterTests.cs
@@ -11,8 +11,24 @@ namespace CoinCombo.Tests
     [TestMethod]
     public void RegisterConstructor_CreatesInstanceOfRegister_Register()
     {
-      Register newRegister = new Register();
+      Register newRegister = new Register(72);
       Assert.AreEqual(typeof(Register), newRegister.GetType());
+    }
+
+    [TestMethod]
+    public void RegisterConstructor_StoresChangeAmt_Register()
+    {
+      int expected = 72;
+      Register newRegister = new Register(72);
+      Assert.AreEqual(expected, newRegister.Change);
+    }
+
+    [TestMethod]
+    public void CoinCounter_CountsNumberOfCoinsEqualToChange_List()
+    {
+      List<int> expected = new List<int> {2, 2, 0, 2};
+      Register newRegister = new Register(72);
+      CollectionAssert.AreEqual(expected, newRegister.CoinCounter());
     }
   }
 }
--- CoinCombo/Models/Register.cs
+++ CoinCombo/Models/Register.cs
@@ -1,14 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 namespace CoinCombo.Models
 {
   public class Register
   {
     public int Change { get; set; }
+    public static List<int> Coins = new List<int> {25, 10, 5, 1};
     
-    // public static int Remainder()
-    // {
-      
-    // }
+    public Register(int change)
+    {
+      Change = change;
+    }
+    public List<int> CoinCounter()
+    {
+      List<int> coinAmts = new List<int> {};
+      int remainingChange = this.Change;
+      foreach (int coin in Coins)
+      {
+      int coinAmt = (int)(remainingChange / coin);
+      coinAmts.Add(coinAmt);
+      remainingChange = remainingChange % coin;
+      }
+      return coinAmts;
+    }
   }
 }
