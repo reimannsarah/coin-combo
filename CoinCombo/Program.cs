caf4ed650312b04b357bbca6566bc33d6ce32a95
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-02 14:33:24 -0700

    add base project structure
     Co-authored-by: Kyle Crawford <kcraw217@gmail.com>

---
+++ CoinCombo/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using CoinCombo.Models;
+using System;
+using CoinCombo.Models;
+using CoinCombo.UserInterfaceModels;
+
+namespace CoinCombo
+{
+  class Program
+  {
+    static void Main()
+    {
+      //
+    }
+  }
+}
